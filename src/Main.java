6adea867e64f327cc1207d793ba6fed9c9e3a1ed
Alex Almansa <alex.almansa.2016@salleurl.edu>
2018-03-29 13:04:00 +0200

    Ja esta feta la conexió amb mysql

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,28 @@
+import java.sql.*;
+
 public class Main {
 
 
     //prova
 
     public static void main(String[] args) {
-        System.out.println("yey");
-    }
+
+        String url = "jdbc:mysql://localhost:3306/LSRestaurant";
+        String username = "root";
+        String password = "alex";
+
+        System.out.println("Connecting database...");
+
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            System.out.println("Database connected!");
+            DatabaseMetaData m = connection.getMetaData();
+            ResultSet tables = m.getTables(connection.getCatalog(), null, "TAB_%", null);
+            for (int i = 1; i < tables.getMetaData().getColumnCount(); i++) {
+                System.out.println("table = " + tables.getMetaData().getTableName(i));
+            }
+        } catch (SQLException e) {
+            throw new IllegalStateException("Cannot connect the database!", e);
+        }    }
 
     //funciona
 

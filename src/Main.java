27b9320efbdf607d452e6a744e19c09e4ea99770
Alex Almansa <alex.almansa.2016@salleurl.edu>
2018-03-29 13:04:42 +0200

    Ja esta feta la conexió amb mysql

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) {
 
         String url = "jdbc:mysql://localhost:3306/LSRestaurant";
+
         String username = "root";
         String password = "alex";
 

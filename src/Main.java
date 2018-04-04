ab33cead42ce2923b7857c4328b60ac65c8b36b7
Marc Llort Maulion <mac12llm@gmail.com>
2018-04-04 08:47:59 +0200

    Server

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,34 @@ import Model.BDD;
 
 import java.sql.*;
 
+import Controlador.Controlador;
+import Model.Comandador;
+import Network.Server;
+import Vista.ServidorVista;
+
 public class Main {
-
-
-    //prova
 
     public static void main(String[] args) {
 
+        Serverbbdd();
 
+        ServidorVista vista = new ServidorVista();          //Creo vista del server
+        vista.setVisible(true);                             //La fem visible
+
+        Comandador comandador = new Comandador();           //Creo el model del servidor
+
+        Server servidor = new Server(comandador);           //Creo servidor que conte el model, per poder enviar i guardar dades
+
+        Controlador controlador = new Controlador(vista, comandador, servidor);             //Creo controlador que conte el model, vista i servidor per poder controlarlos quan toqui
+
+        vista.registraControlador(controlador);             //Registro els elements de la vista al controlador
+
+        servidor.startServer();                             //Inicio servidor
+
+    }
+
+
+    public static void Serverbbdd(){
         try {
             BDD bdd = new BDD();
             String s = "ALL";
@@ -23,7 +43,12 @@ public class Main {
             System.out.println("ERROOR");
             e.printStackTrace();
         }
-
-        //funciona
     }
+
 }
+
+
+
+
+
+

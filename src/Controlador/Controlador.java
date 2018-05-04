ee44dd867f00dd91122fc4107c8fb020bd0649e8
Marc Llort Maulion <mac12llm@gmail.com>
2018-05-04 08:46:17 +0200

    s

--- src/Controlador/Controlador.java
+++ src/Controlador/Controlador.java
@@ -11,6 +11,7 @@ import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 
 
@@ -146,9 +147,9 @@ public class Controlador implements ActionListener {
 
             }
             if (e.getActionCommand().equals("SERVIR TAULA")) {
-                //panel.registraControladorDialog(this);
                 String fila = vistaComanda.filaSeleccionada();
                 if (!fila.equals("null")) {
+                    panel.dispatchEvent(new WindowEvent(panel, WindowEvent.WINDOW_CLOSING));
                     panel = new VistaServirComandes(gestionador, fila);
                     panel.registraControladorDialog(this);
                     panel.setVisible(true);

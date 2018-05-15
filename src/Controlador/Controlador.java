a61dac984204b31ea9d7377bef25567627d15b9b
Marc <marc.llort.2016@salleurl.edu>
2018-05-15 10:28:39 +0200

    Server tot menys plats

--- src/Controlador/Controlador.java
+++ src/Controlador/Controlador.java
@@ -149,13 +149,14 @@ public class Controlador implements ActionListener {
             } catch (NumberFormatException ne) {
                 vista.showError("Error! Caracters no permesos a preu i unitats!");
                 dialogAfegirPlat.netejaCamps();
-                ne.printStackTrace();
+                //ne.printStackTrace();
             }
         }
         if (e.getActionCommand().equals("ACTUALITZAR")) {
             System.out.println("actualizat " + boto);
             try {
                 gestionador.afegeixUnitats(boto, dialogUpdatePlat.getJtfUnitats());
+                sReserva.enviaCarta();
                 vista.showError("Unitats de " + boto + " actualitzades!");
                 dialogUpdatePlat.dispatchEvent(new WindowEvent(dialogUpdatePlat, WindowEvent.WINDOW_CLOSING));
                 dialogUpdatePlat.netejaCamps();
